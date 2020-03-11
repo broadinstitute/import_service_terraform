334f116975df1cbd5511b4388000db6acddb62c2
Hussein Elgridly <hussein@broadinstitute.org>
2020-03-11 13:20:50 -0400

    provider

--- import-service-local/main.tf
+++ import-service-local/main.tf
@@ -7,8 +7,11 @@ module "import-service" {
   import_service_google_project_folder_id = var.import_service_google_project_folder_id
   billing_account_id = var.billing_account_id
 
-
-  terraform_google_project = var.terraform_google_project
   audience_domain = var.audience_domain
   rawls_sa_email = var.rawls_sa_email
+
+  providers {
+    google.target = google
+    google-beta.target = google-beta
+  }
 }
