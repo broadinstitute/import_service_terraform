023c830c52db9ba2392e1f21ceb966eccc251e3d
David An <davidan@broadinstitute.org>
2020-05-08 12:11:05 -0400

    rename deploy to deployer

--- import-service/google-project.tf
+++ import-service/google-project.tf
@@ -17,8 +17,8 @@ module "import-service-project" {
       sa_name = "import-service"
       key_vault_path = "${var.vault_root}/${local.vault_path}/import-service-account.json"
     },{
-      sa_name = "deploy"
-      key_vault_path = "${var.vault_root}/${local.vault_path}/deploy.json"
+      sa_name = "deployer"
+      key_vault_path = "${var.vault_root}/${local.vault_path}/deployer.json"
     }
   ]
 
@@ -38,7 +38,7 @@ module "import-service-project" {
     sa_project = "" // defaults to the created project
   },{
     sa_role = "roles/appengine.deployer"
-    sa_name = "deploy"
+    sa_name = "deployer"
     sa_project = "" // defaults to the created project
   }]
 
