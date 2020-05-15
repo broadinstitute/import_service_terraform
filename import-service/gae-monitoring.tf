e2cce7865949ff32125863b03054a02d0ea343bb
Mike Flinn <mflinn@broadinstitute.org>
2020-05-15 11:23:34 -0400

    Bump monitoring module version to include service name fixes

--- import-service/gae-monitoring.tf
+++ import-service/gae-monitoring.tf
@@ -1,8 +1,8 @@
 module "gae_monitoring" {
-  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/stackdriver/gae-monitoring?ref=gae-monitoring-0.0.1-tf-0.12"
+  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/stackdriver/gae-monitoring?ref=gae-monitoring-0.0.2-tf-0.12"
   providers = {
     google.target = google.target
   }
-  service_name     = var.service
+  service_name     = "importservice-${var.env}"
   gae_host_project = var.import_service_google_project
 }
