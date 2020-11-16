0c2c6d1e26264350444d18e3974788ba1585a4c8
mflinn-broad <mflinn@broadinstitute.org>
2020-11-16 16:02:51 -0500

    still trying to access rawls ip

--- import-service/firewall.tf
+++ import-service/firewall.tf
@@ -13,15 +13,15 @@ resource "google_app_engine_firewall_rule" "broad_firewall" {
 
 data "google_compute_instance" "back_rawls" {
   name    = local.back_rawls_instance_name
-  project = "broad-dsde-dev"
+  project = "broad-dsde-${var.env}"
   zone    = "us-central1-a"
 }
 
-resource "google_app_engine_firewall_rule" "back_rawls" {
+resource "google_app_engine_firewall_rule" "back_rawls_firewall" {
   project      = google_app_engine_application.gae_import_service.project
   priority     = 1000 + length(var.broad_range_cidrs)
   action       = "ALLOW"
-  source_range = data.google_compute_instance.back_rawls.network_interface.access_config.nat_ip
+  source_range = data.google_compute_instance.back_rawls.*.network_interface.0.access_config.nat_ip
 }
 
 # default-deny firewall rule
