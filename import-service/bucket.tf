3cc2ebea2159c69c92306de67c3689ebbb5ea8fa
Matt Bemis <MatthewBemis@users.noreply.github.com>
2021-08-26 14:28:46 -0400

    add rawls SA as writer on bucket

--- import-service/bucket.tf
+++ import-service/bucket.tf
@@ -20,6 +20,15 @@ resource "google_storage_bucket_iam_binding" "import_service_owns_batchupsert_bu
   ]
   depends_on = [module.import-service-project.service_accounts_with_keys]
 }
+  
+resource "google_storage_bucket_iam_binding" "rawls_writes_batchupsert_bucket" {
+  bucket = google_storage_bucket.batchupsert_bucket.name
+  role = "roles/storage.legacyBucketWriter"
+  members = [
+      "serviceAccount:${var.rawls_sa_email}",
+  ]
+  depends_on = [module.import-service-project.service_accounts_with_keys]
+}
 
 # Note that default object ACLs aren't visible in cloud console.
 # gsutil defacl get gs://yourbucket will show you.
