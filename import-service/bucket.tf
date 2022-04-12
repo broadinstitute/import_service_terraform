b66060293e921e5bd617735073e58208e1382d56
Drew Hite <drew@drewhite.org>
2022-04-12 18:04:39 -0400

    saving this here for now

--- import-service/bucket.tf
+++ import-service/bucket.tf
@@ -21,6 +21,17 @@ resource "google_storage_bucket_iam_binding" "import_service_owns_batchupsert_bu
   ]
   depends_on = [module.import-service-project.service_accounts_with_keys]
 }
+
+resource "google_storage_bucket_fiab_iam_binding" "fiab_owns_batchupsert_bucket_in_qa" {
+  bucket = google_storage_bucket.batchupsert_bucket.name
+  role = "roles/storage.admin"
+  members = [
+    "serviceAccount:import-service-fiab@broad-dsde-qa.iam.gserviceaccount.com",
+  ]
+  depends_on = [module.import-service-project.service_accounts_with_keys]
+  count = var.env == 'qa' ? 1 : 0
+}
+
   
 resource "google_storage_bucket_iam_binding" "rawls_creator_batchupsert_bucket" {
   bucket = google_storage_bucket.batchupsert_bucket.name
