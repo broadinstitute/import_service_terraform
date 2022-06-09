0ff7914fa0510539c4ed5fe538ae5c8cb7cc2b35
David An <davidan@broadinstitute.org>
2022-06-09 14:28:08 -0400

    explicit provider addresses

--- import-service/versions.tf
+++ import-service/versions.tf
@@ -1,22 +1,22 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source = "registry.terraform.io/hashicorp/google"
     }
     google-beta = {
-      source = "hashicorp/google-beta"
+      source = "registry.terraform.io/hashicorp/google-beta"
     }
     http = {
-      source = "hashicorp/http"
+      source = "registry.terraform.io/hashicorp/http"
     }
     null = {
-      source = "hashicorp/null"
+      source = "registry.terraform.io/hashicorp/null"
     }
     random = {
-      source = "hashicorp/random"
+      source = "registry.terraform.io/hashicorp/random"
     }
     vault = {
-      source = "hashicorp/vault"
+      source = "registry.terraform.io/hashicorp/vault"
     }
   }
   required_version = ">= 0.13"
